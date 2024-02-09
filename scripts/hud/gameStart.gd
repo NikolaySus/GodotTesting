af1e122978dfe457027df7f2202256fd57fe3325
NikolaySus <gorkunovnm@gmail.com>
2024-02-10 00:53:54 +0300

    Revert "Merge remote-tracking branch 'origin/scene-manager' into dev"
    This reverts commit c31715b35cd11e4a64557889d68ba89e0f736026, reversing
    changes made to 5d6e1a7eb671c13f10c45cde677989cf9a7b33a9.

--- scripts/hud/gameStart.gd
+++ scripts/hud/gameStart.gd
@@ -7,8 +7,10 @@ func set_visibility(val):
 	self.visible = val
 
 func _on_continue_button_pressed():
+	#$".".get_child(0).set_visible(false)
 	self.set_visibility(false)
 
 func _on_main_menu_button_pressed():
+	print_debug("Going to main menu")
+	#$".".get_child(0).set_visible(false)
 	self.set_visibility(false)
-	get_tree().change_scene_to_file("res://scenes/pages/mainMenu.tscn")
